5e85efdb3a0400a2e04ddad161ef17903dd7cef0
varoon-fang <FaNg@varoons-MacBook-Pro.local>
2013-09-27 13:50:47 +0700

    add information lanna
    ตั้งชื่อ link ตามชื่อจังหวัด

--- application/controllers/information_lanna.php
+++ application/controllers/information_lanna.php
@@ -15,11 +15,67 @@ class Information_lanna extends CI_Controller {
 		$this->load->view('front/temp/footer');
 	}
 
-	function composer()
+	function nan()
 	{
 
 		$this->load->view('front/temp/header', $this->data);
 		$this->load->view('front/lanna/nan');
+		$this->load->view('front/temp/footer');
+	}
+
+	function phrae()
+	{
+
+		$this->load->view('front/temp/header', $this->data);
+		$this->load->view('front/lanna/phrae');
+		$this->load->view('front/temp/footer');
+	}
+
+	function maehongson()
+	{
+
+		$this->load->view('front/temp/header', $this->data);
+		$this->load->view('front/lanna/maehongson');
+		$this->load->view('front/temp/footer');
+	}
+
+	function phayao()
+	{
+
+		$this->load->view('front/temp/header', $this->data);
+		$this->load->view('front/lanna/phayao');
+		$this->load->view('front/temp/footer');
+	}
+
+	function lampang()
+	{
+
+		$this->load->view('front/temp/header', $this->data);
+		$this->load->view('front/lanna/lampang');
+		$this->load->view('front/temp/footer');
+	}
+
+	function chiangrai()
+	{
+
+		$this->load->view('front/temp/header', $this->data);
+		$this->load->view('front/lanna/chiangrai');
+		$this->load->view('front/temp/footer');
+	}
+
+	function lamphun()
+	{
+
+		$this->load->view('front/temp/header', $this->data);
+		$this->load->view('front/lanna/lamphun');
+		$this->load->view('front/temp/footer');
+	}
+
+	function chiangmai()
+	{
+
+		$this->load->view('front/temp/header', $this->data);
+		$this->load->view('front/lanna/chiangmai');
 		$this->load->view('front/temp/footer');
 	}
 
